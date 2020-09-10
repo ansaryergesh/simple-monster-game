58534f76fa35df94c0699d081912b4c4aaafc950
ansaryergesh <yergeshovansar@gmail.com>
2020-09-10 11:27:21 +0600

    attacking

--- app.js
+++ app.js
@@ -10,6 +10,26 @@ new Vue( {
           this.gameIsRunning = true;
           this.playerHealth = 100;
           this.monsterHealth = 100;
+      },
+      attack: function() {
+        var max = 10;
+        var min = 3;
+        var damage = Math.floor(Math.random() * (max-min) + min);
+        this.monsterHealth -= damage;
+
+        max = 12;
+        min = 5;
+        var damage = Math.floor(Math.random() * (max-min) + min);
+        this.playerHealth -= damage;
+      },
+      specialAttack: function() {
+          
+      },
+      heal: function() {
+
+      },
+      giveUp: function() {
+
       }
   }
 })
